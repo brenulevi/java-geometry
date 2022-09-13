ed60ebf1d29918bb5b5d83619d8ed0c8acd96ba3
Breno Soares <brenosoares4303@gmail.com>
2022-09-13 12:03:20 -0300

    third commit -> have some error in Geometry

--- Geometry.java
+++ Geometry.java
@@ -1,3 +1,15 @@
 public class Geometry {
-  public double pi = 3.1415;
+  public float pi = 3.14159265359f;
+
+  public static double CircleArea(float r) {
+    return pi * Math.pow(r, 2);
+  }
+
+  static double CircumferenceSize(float r) {
+    return 2 * this.pi * r;
+  }
+
+  static double SphereVolume(float r) {
+    return (4/3) * this.pi * Math.pow(r, 3);
+  }
 }
