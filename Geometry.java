e49f51e0a23ca5a5d4c384f86fa9b18e657573af
Breno Soares <brenosoares4303@gmail.com>
2022-09-14 11:56:48 -0300

    Reformuled

--- Geometry.java
+++ Geometry.java
@@ -1,19 +1,3 @@
 public class Geometry {
   public static double pi = 3.14159265359d;
-
-  public static double CircleArea(float r) {
-    return pi * Math.pow(r, 2);
-  }
-
-  static double CircumferenceSize(float r) {
-    return 2 * pi * r;
-  }
-
-  static double SphereVolume(float r) {
-    return (4/3) * pi * Math.pow(r, 3);
-  }
-
-  static double CilinderVolume(float radius, float height) {
-    return CircleArea(radius) * height; 
-  }
 }
--- Main.java
+++ Main.java
@@ -1,11 +1,50 @@
 public class Main {
   public static void main(String[] args) {
+    Coord3 v1 = Coord3.zero;
+    Coord3 v2 = new Coord3(1f, 1f, 2f);
 
-    Vector3 v1 = new Vector3(2f, 3f, 5f);
-    Vector3 v2 = new Vector3(7f, 4f, 2f);
-    Vector3 v3 = new Vector3(1f, 1f, 1f);
+    Debug.Log(v1.magnitude);
+    Debug.Log(v2.magnitude);
 
-    Debug.Log(Vector3.Subtract(v1, v2, v3));
+    Debug.Log("\n");
 
+    Debug.Log(v1.normalized);
+    Debug.Log(v2.normalized);
+  }
+
+  public void Polygons() {
+    Circle circle = new Circle(5f);
+    Debug.Log("Circle");
+    Debug.Log("Area: " + circle.Area() + "\nPerimeter: " + circle.Perimeter());
+
+    System.out.print("\n");
+    // ================================= //
+
+    Triangle triangle = new Triangle(9, 7, 14);
+    Debug.Log("Triangle");
+    Debug.Log("Area: " + triangle.Area() + "\nPerimeter: " + triangle.Perimeter());
+
+    System.out.print("\n");
+    // ================================= //
+
+    Square square = new Square(4);
+    Debug.Log("Square");
+    Debug.Log("Area: " + square.Area() + "\nPerimeter: " + square.Perimeter());
+
+    System.out.print("\n");
+    // ================================= //
+
+    Rectangle rectangle = new Rectangle(4, 2);
+    Debug.Log("Rectangle");
+    Debug.Log("Area: " + rectangle.Area() + "\nPerimeter: " + rectangle.Perimeter());
+
+    System.out.print("\n");
+    // ================================= //
+
+    Pentagon pentagon = new Pentagon(4);
+    Debug.Log("Pentagon");
+    Debug.Log("Area: " + pentagon.Area() + "\nPerimeter: " + pentagon.Perimeter());
+
+    System.out.print("\n");
   }
 }
