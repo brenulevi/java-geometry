d6c296eac786a1299c9c47caf0121a83fe9d159c
Breno Soares <brenosoares4303@gmail.com>
2022-09-15 11:38:46 -0300

    added comments to Vector3 class

--- Main.java
+++ Main.java
@@ -5,7 +5,6 @@ public class Main {
   public static void main(String[] args) {
     Vector3 v1 = new Vector3(0f, 1f, 2f);
     Vector3 v2 = new Vector3(1f, 10f, 9f);
-    Vector3 v3 = new Vector3(5f, 2f, -4f);
 
     Debug.Log(Vector3.CrossProduct(v1, v2));
   }
--- src/Vector3.java
+++ src/Vector3.java
@@ -2,7 +2,12 @@ package src;
 
 public class Vector3 {
   public final float x, y, z;
+
+  /**
+   * Return the vector magnitude
+   */
   public float magnitude;
+
   /**
    * Return the normalized Vector3 between 0 and 1
    */
@@ -28,24 +33,29 @@ public class Vector3 {
 
   /**
    * Calculate the distance between Vector3 a and Vector3 b
-   * 
-   * @param a - Vector3
-   * @param b - Vector3
-   * @return float - Distance between Vector3 a and Vector3 b
    */
   public static float Distance(Vector3 a, Vector3 b) {
     return (float) Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2) + Math.pow(b.z - a.z, 2));
   }
 
+  /**
+   * Calculate the cross product between Vector3 a and Vector3 b
+   */
   public static Vector3 CrossProduct(Vector3 a, Vector3 b) {
     return new Vector3((a.y * b.z) - (a.z * b.y), (a.z * b.x) - (a.x * b.z), (a.x * b.y) - (a.y * b.x));
   }
 
+  /**
+   * Calculate the scalar product between Vector3 a and Vector3 b
+   */
   public static float ScalarProduct(Vector3 a, Vector3 b) {
     return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
   }
 
-  public static Vector3 Add(Vector3... c) {
+  /**
+   * Method to perform a sum between n Vector3's
+   */
+  public static Vector3 Sum(Vector3... c) {
     float sum_x = c[0].x, sum_y = c[0].y, sum_z = c[0].z;
 
     Vector3[] c_copy = new Vector3[c.length - 1];
@@ -65,6 +75,9 @@ public class Vector3 {
     return new Vector3(sum_x, sum_y, sum_z);
   }
 
+  /**
+   * Method to perform a subtraction between n Vector3's
+   */
   public static Vector3 Subtract(Vector3... c) {
     float sum_x = c[0].x, sum_y = c[0].y, sum_z = c[0].z;
 
